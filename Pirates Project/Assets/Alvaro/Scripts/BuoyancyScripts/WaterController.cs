b958b8c36bdf832743950a2044b26a1695da0e76
Álvaro Martínez Soto <halvaren@users.noreply.github.com>
2019-03-08 20:29:13 +0100

    Intentos sin éxito de meterle shader cartoon al agua
    La textura principal parece estar super estirada sobre los cuadros del agua, y no se aprecia la textura

--- Pirates Project/Assets/Alvaro/Scripts/BuoyancyScripts/WaterController.cs
+++ Pirates Project/Assets/Alvaro/Scripts/BuoyancyScripts/WaterController.cs
@@ -48,4 +48,14 @@ public class WaterController : MonoBehaviour
 
         return distanceToWater;
     }
+
+    void Update()
+    {
+        /* Shader.SetGlobalFloat("_WaterScale", scale);
+        Shader.SetGlobalFloat("_WaterSpeed", speed);
+        Shader.SetGlobalFloat("_WaterDistance", waveDistance);
+        Shader.SetGlobalFloat("_WaterTime", Time.time);
+        Shader.SetGlobalFloat("_WaterNoiseStrength", noiseStrength);
+        Shader.SetGlobalFloat("_WaterNoiseWalk", noiseWalk);*/
+    }
 }
