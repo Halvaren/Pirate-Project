4c0730b4bbb3037d0256b841a05c4ce1dc3c0f17
Álvaro Martínez Soto <halvaren@users.noreply.github.com>
2019-03-08 14:34:20 +0100

    Barco ejemplo modificado para que no traspase el agua, cámara giratoria añadida
    Falta añadir la cámara aérea, el cambio entre ellas y la espuma

---
+++ Pirates Project/Assets/Alvaro/Scripts/FollowBoatCam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBoatCam : MonoBehaviour
+{
+    public float DistanceFromBoat;
+    public float RotationSpeed;
+    public Transform BoatTransform;
+    public float XAngle;
+
+    private float yAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yAngle = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            yAngle += RotationSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(XAngle, yAngle * Mathf.Rad2Deg, transform.rotation.z);
+        transform.position = new Vector3(BoatTransform.position.x - DistanceFromBoat * Mathf.Sin(yAngle), transform.position.y, BoatTransform.position.z - DistanceFromBoat * Mathf.Cos(yAngle));
+    }
+}
