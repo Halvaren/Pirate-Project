410a2cf83e238e657e847a53f63f9b215ca350f8
ScampiStar <44586245+ScampiStar@users.noreply.github.com>
2019-03-12 19:01:35 +0100

    Movimiento personaje

--- Pirates Project/Assets/Miguel/Assets/Pruebas/Scripts/PlayerMovement.cs
+++ Pirates Project/Assets/Miguel/Assets/Pruebas/Scripts/PlayerMovement.cs
@@ -40,10 +40,10 @@ public class PlayerMovement : MonoBehaviour
     void Move (float h, float v)
     {
         // Set the movement vector based on the axis input.
-        movement.Set (h, 0f, v);
+        //movement.Set (h, 0f, v);
         
         // Normalise the movement vector and make it proportional to the speed per second.
-        movement = movement.normalized * speed * Time.deltaTime;
+        movement = (transform.forward * v + transform.right * h) * speed * Time.deltaTime;
 
         // Move the player to it's current position plus the movement.
         playerRigidbody.MovePosition (transform.position + movement);
@@ -77,9 +77,12 @@ public class PlayerMovement : MonoBehaviour
     void Animating (float h, float v)
     {
         // Create a boolean that is true if either of the input axes is non-zero.
-        bool walking = h != 0f || v != 0f;
+        bool walking = v != 0f;
+        bool rotating = Input.GetAxis("Mouse X") != 0f || Input.GetAxis("Mouse Y") != 0f;
+        bool sideRunning = h != 0f;
 
         // Tell the animator whether or not the player is walking.
-        anim.SetBool ("IsWalking", walking);
+        anim.SetBool ("IsWalking", (walking || rotating) && !sideRunning);
+        anim.SetBool ("IsSideRunning", sideRunning);
     }
 }
--- Pirates Project/Assets/Miguel/Assets/Survival Shooter/Scripts/Player/PlayerMovement.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace DuplicatedScripts
-{
-    public class PlayerMovement : MonoBehaviour
-    {
-    
-    }
-}
