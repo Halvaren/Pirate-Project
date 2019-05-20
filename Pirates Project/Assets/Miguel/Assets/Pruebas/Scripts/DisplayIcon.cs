53d88fca492bf1effa821e2bd0b4a1ae21c086d0
ScampiStar <44586245+ScampiStar@users.noreply.github.com>
2019-05-20 19:10:06 +0200

    Mapa casi terminado

---
+++ Pirates Project/Assets/Miguel/Assets/Pruebas/Scripts/DisplayIcon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayIcon : MonoBehaviour
+{
+    private SpriteRenderer rend;
+    public Color visibleColor;
+    public Transform playerMapIcon;
+
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, playerMapIcon.position) < 3f)
+        {
+            Debug.Log("es visible");
+            rend.color = visibleColor;
+        }
+    }
+}
