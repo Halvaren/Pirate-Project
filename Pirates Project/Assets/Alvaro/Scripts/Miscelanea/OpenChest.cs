e568a3455a291074eafd616b8e17edd49a8c8304
Sabina <sabinamrcc@gmail.com>
2019-06-04 22:43:12 +0200

    con la animacion del cofre

--- Pirates Project/Assets/Alvaro/Scripts/Miscelanea/OpenChest.cs
+++ Pirates Project/Assets/Alvaro/Scripts/Miscelanea/OpenChest.cs
@@ -10,6 +10,8 @@ public class OpenChest : MonoBehaviour
     // Start is called before the first frame update
     //public Camera mainCam;
     //public Camera rotatingCam;
+    private bool nearChest;
+    private bool spacePressed;
     void Start()
     {
         
@@ -18,14 +20,16 @@ public class OpenChest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space)) spacePressed = true;
     }
     
     private void OnTriggerEnter (Collider other) {
-        if (other.gameObject.tag == "Player") {
-           
-             GameManager.Instance.LocalPlayer.stopInput = true; 
+        if ((other.gameObject.tag == "Player") && (spacePressed)/*&& (Input.GetKeyDown(KeyCode.Space))*/) {
+            //nearChest = true; 
+           //if() print("cosas");
+             GameManager.Instance.LocalPlayer.finishedGame = true; 
               Debug.Log("ey");
+            other.gameObject.active = false;
             GetComponent<Animator>().SetBool("isOpening", true);
             
            /* if (other.GetComponent<PlayerHealthController>().HasKey()) {
