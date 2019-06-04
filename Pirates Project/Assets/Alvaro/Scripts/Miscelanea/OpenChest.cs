9559962a1c0fbce49bdc4af185d93d70767953e9
Sabina <sabinamrcc@gmail.com>
2019-06-04 18:27:02 +0200

    lo del final

---
+++ Pirates Project/Assets/Alvaro/Scripts/Miscelanea/OpenChest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DefinitiveScript;
+using Cinemachine;
+
+public class OpenChest : MonoBehaviour
+{
+    //public GameObject player; 
+    // Start is called before the first frame update
+    //public Camera mainCam;
+    //public Camera rotatingCam;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter (Collider other) {
+        if (other.gameObject.tag == "Player") {
+           
+             GameManager.Instance.LocalPlayer.stopInput = true; 
+              Debug.Log("ey");
+            GetComponent<Animator>().SetBool("isOpening", true);
+            
+           /* if (other.GetComponent<PlayerHealthController>().HasKey()) {
+                GameManager.Instance.LocalPlayer.stopInput = true; 
+        
+            }
+           */
+        }
+
+    }
+}
